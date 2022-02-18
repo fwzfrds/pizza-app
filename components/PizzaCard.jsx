af0a727b91d984c93b3cbb321160d654cd2cdbe2
Fawwaz Firdaus <muhammadfawwaz463@gmail.com>
2022-02-18 14:29:51 +0700

    menambah fitur klik pada pizza card

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,9 +1,14 @@
 import styles from "../styles/PizzaCard.module.css"
 import Image from "next/image"
+import { useRouter } from "next/router"
+
 
 const PizzaCard = () => {
+
+    const router = useRouter();
+
     return (
-        <div className={styles.container}>
+        <div className={styles.container} onClick={() => router.push(`/cart`)} >
             <Image src="/img/pizza.png" alt="" width="500" height="500" />
             <h1 className={styles.title}>FIORI DI ZUCCA</h1>
             <span className={styles.price}>$19.90</span>
